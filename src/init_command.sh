246e1fa4761c51afd2513fd47d0a19c818eb8188
Turtle-In-Rock <turtle.in.rock@gmail.com>
2025-06-27 09:51:51 -0400

    add bashly to script

---
+++ src/init_command.sh
@@ -0,0 +1,38 @@
+print_info() {
+  echo "$(blue [*])" "$1"
+}
+
+has_match() {
+  local pattern="$1"
+  local file="$2"
+
+  if  rg -q "$pattern" "$file" ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+name=${args[--name]}
+
+mkdir $name
+
+# Create .env
+cd $name
+theia_set_command
+cd ..
+
+source $name/$name.env
+
+print_info "starting nmap scan..."
+nmap -sC -sV -T4 -p- $ip -oX $name/port-scan.xml -oN $name/port-scan.nmap
+
+if has_match "http " $name/port-scan.txt ; then
+  print_info "starting ffuf scan..."
+  ffuf -u $url/FUZZ -w /usr/share/seclists/Discovery/Web-Content/big.txt -recursion -t 100 -of md -o $name/web-dir.md
+  firefox $ip
+else
+  print_info "found no web port"
+fi
+
+print_info "all scans complete."
---
+++ src/set_command.sh
@@ -0,0 +1,17 @@
+ip=${args[ipaddr]}
+name=${args[--name]}
+host=${args[--hostname]}
+ext=${args[--extension]}
+env_file="$name.env"
+
+echo "# Created by Theia" >> $env_file
+
+echo "ip=$ip" >> $env_file
+if [ -z $host ] ; then
+  url="http://$ip"
+else
+  url="http://$host.$ext"
+  echo "host=$host" >> $env_file
+fi
+
+echo "url=$url" >> $env_file
