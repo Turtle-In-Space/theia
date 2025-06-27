2d3a4cad8cda8078ebba282100a46ba24a5de104
Turtle-In-Rock <turtle.in.rock@gmail.com>
2025-06-27 13:32:37 -0400

    dont remember

--- src/init_command.sh
+++ src/init_command.sh
@@ -15,8 +15,10 @@ has_match() {
 
 name=${args[--name]}
 
+print_info "creating $name/..."
 mkdir $name
 
+print_info "creating .env file"
 # Create .env
 cd $name
 theia_set_command
@@ -27,12 +29,23 @@ source $name/$name.env
 print_info "starting nmap scan..."
 nmap -sC -sV -T4 -p- $ip -oX $name/port-scan.xml -oN $name/port-scan.nmap
 
-if has_match "http " $name/port-scan.txt ; then
+# enum web service shares
+if has_match "http" $name/port-scan.nmap ; then
   print_info "starting ffuf scan..."
-  ffuf -u $url/FUZZ -w /usr/share/seclists/Discovery/Web-Content/big.txt -recursion -t 100 -of md -o $name/web-dir.md
-  firefox $ip
+  ffuf -u $url/FUZZ -w /usr/share/seclists/Discovery/Web-Content/big.txt -recursion -recursion-depth 2 -t 100 -of md -o $name/web-dir.md
+  firefox $url
 else
   print_info "found no web port"
 fi
 
+# enum smb shares
+if has_match "netbios-ssn" $name/port-scan.nmap ; then
+  print_info "startig enum4linux-ng scan..."
+  enum4linux-ng -As $ip -oJ smb-enum
+else
+  print_info "found no smb ports"
+fi
+
 print_info "all scans complete."
+print_info "add the following to /etc/hosts"
+echo $ip $host.$ext
