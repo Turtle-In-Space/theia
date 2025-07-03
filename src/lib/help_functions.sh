85a23a5a8ba0a2bf3757b9c2285b8ad1f41d6d59
Turtle-In-Space <112472302+Turtle-In-Space@users.noreply.github.com>
2025-07-03 12:36:47 +0200

    Update help_functions.sh

--- src/lib/help_functions.sh
+++ src/lib/help_functions.sh
@@ -1,11 +1,15 @@
+print_ok() {
+  echo "$(green [+])" "$1"
+}
+
 print_info() {
   echo "$(blue [*])" "$1"
 }
 
 print_warn() {
-  echo "$(orange [!])" "$1"
+  echo "$(orange [-])" "$1"
 }
 
-print_ok() {
-  echo "$(green [!])" "$1"
+print_err() {
+  echo "$(red [!])" "$1"
 }
