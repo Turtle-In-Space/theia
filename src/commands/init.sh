62373f65325b82fbbd2a18178b1a24e786853e98
Turtle-In-Space <112472302+Turtle-In-Space@users.noreply.github.com>
2025-07-04 16:17:43 +0200

    fix string usage

--- src/commands/init.sh
+++ src/commands/init.sh
@@ -26,30 +26,30 @@ port_scan=export/port-scan.xml
 
 # Create dir
 print_info "creating $name/..."
-mkdir -p $name/export
-cd $name
+mkdir -p "$name"/export
+cd "$name"
 
 # Create .env
-print_info "creating .env file"
+print_info 'creating .env file'
 theia_set_command
-source $env_file
+source "$env_file"
 
 # scan target
-print_info "starting nmap scan..."
-nmap -sC -sV -T4 -p- $ip -oX $port_scan -oN port-scan.nmap
+print_info 'starting nmap scan...'
+nmap -sC -sV -T4 -p- "$ip" -oX "$port_scan" -oN port-scan.nmap
 
 # script commands
 http_scan_command="ffuf -u $url/FUZZ -w /usr/share/seclists/Discovery/Web-Content/big.txt -recursion -recursion-depth 1 -t 100 -of md -o web-dir.md"
 smb_scan_command="enum4linux-ng -As $ip -oJ smb-enum"
 
 # run enum scripts
-run_script "http" $http_scan_command
-run_script "netbios-ssn" $smb_scan_command
+run_script 'http' "$http_scan_command"
+run_script 'netbios-ssn' "$smb_scan_command"
 
-print_ok "all scans complete."
+print_ok 'all scans complete.'
 
 if [ -z $host ] ; then
-  echo ""
-  print_info "add the following to /etc/hosts"
-  echo $ip $host.$ext
+  echo ''
+  print_info 'add the following to /etc/hosts'
+  echo "$ip" "$host.$ext"
 fi
