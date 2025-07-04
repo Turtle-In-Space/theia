9367bd3a6e0c2991c3357988ae71e4d0425fe28f
Turtle-In-Space <112472302+Turtle-In-Space@users.noreply.github.com>
2025-07-04 16:18:32 +0200

    fix string usage

--- src/commands/set.sh
+++ src/commands/set.sh
@@ -4,16 +4,16 @@ host=${args[--hostname]}
 ext=${args[--extension]}
 env_file="$name.env"
 
-echo "# Created by Theia" >> $env_file
+echo '# Created by Theia' >> $env_file
 
-echo "ip=$ip" >> $env_file
+echo "ip=$ip" >> "$env_file"
 if [ -z $host ] ; then
   url="http://$ip"
 else
   url="http://$host.$ext"
-  echo "host=$host" >> $env_file
+  echo "host=$host" >> "$env_file"
 fi
 
-echo "url=$url" >> $env_file
+echo "url=$url" >> "$env_file"
 
 print_ok "Created $env_file"
