5445a49f03947b203fa7bee0fef37d276c85a2fc
Turtle-In-Space <elias.svensson63@gmail.com>
2025-07-08 12:17:30 +0200

    shorten copyright notice in files

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,10 +1,11 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Elias Svensson <elias.svensson63@gmail.com>
 */
 package cmd
 
 import (
+	msg "github.com/Turtle-In-Space/theia/internal/text/cmd/scan"
+
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -12,14 +13,10 @@ import (
 
 // scanCmd represents the scan command
 var scanCmd = &cobra.Command{
-	Use:   "scan",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   msg.Usage,
+	Short: msg.Short,
+	Long:  msg.Long,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("scan called")
 	},
