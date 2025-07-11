d337ba78c68b5d1c09cae0c30f0d1a4bcb42130d
Turtle-In-Space <112472302+Turtle-In-Space@users.noreply.github.com>
2025-07-11 10:20:42 +0200

    add success, warn and error print
    move highlighting to sep func

--- pkg/output/terminal.go
+++ pkg/output/terminal.go
@@ -8,8 +8,36 @@ func Info(msg string, args ...any) {
 	pterm.Info.Prefix = pterm.Prefix{Text: "INFO", Style: pterm.NewStyle(pterm.BgBlue, pterm.FgBlack)}
 	pterm.Info.MessageStyle = pterm.NewStyle(pterm.FgBlue)
 
-	style := pterm.NewStyle(pterm.BgYellow, pterm.FgBlack, pterm.Bold)
+	highlightedMsg := highlightArgs(msg, args...)
+	
+	// Print the final formatted message with styled args
+	pterm.Info.Printfln(highlightedMsg, styledArgs...)
+}
 
+func Success(msg string, args ...any) {
+	highlightedMsg := highlightArgs(msg, args...)
+
+	// Print the final formatted message with styled args
+	pterm.Info.Printfln(highlightedMsg, styledArgs...)
+}
+
+func Warn(msg string, args ...any) {
+	highlightedMsg := highlightArgs(msg, args...)
+
+	// Print the final formatted message with styled args
+	pterm.Warning.Printfln(highlightedMsg, styledArgs...)
+}
+
+func Error(msg string, args ...any) {
+	highlightedMsg := highlightArgs(msg, args...)
+
+	// Print the final formatted message with styled args
+	pterm.Error.Printfln(highlightedMsg, styledArgs...)
+}
+
+func highlightArgs(msg string, args ...any) highlightedMsg string {
+	style := pterm.NewStyle(pterm.BgYellow, pterm.FgBlack, pterm.Bold)
+	
 	// Apply the style to each argument
 	styledArgs := make([]any, len(args))
 	for i, arg := range args {
@@ -19,7 +47,8 @@ func Info(msg string, args ...any) {
 	// As all args are now strings replace format to match
 	regEx := regexp.MustCompile(`%[a-zA-Z]`)
 	format := regEx.ReplaceAllString(msg, "%s")
+	
+	highlightedMsg = fmt.Sprintf(format, args...)
 
-	// Print the final formatted message with styled args
-	pterm.Info.Printfln(format, styledArgs...)
+	return 
 }
