b32c6b918a59cfbcf0351724a3f94202f98da234
Turtle-In-Space <elias.svensson63@gmail.com>
2025-07-14 00:40:08 +0200

    fix format errors

--- pkg/output/terminal.go
+++ pkg/output/terminal.go
@@ -8,30 +8,36 @@ import (
 	"github.com/pterm/pterm"
 )
 
+var highlightedStyle *pterm.Style
+
 func init() {
+	highlightedStyle = pterm.NewStyle(pterm.BgYellow, pterm.FgBlack, pterm.Bold)
+
 	pterm.Info.Prefix = pterm.Prefix{Text: "INFO", Style: pterm.NewStyle(pterm.BgBlue, pterm.FgBlack)}
 	pterm.Info.MessageStyle = pterm.NewStyle(pterm.FgBlue)
+
+	pterm.EnableDebugMessages()
 }
 
 func Info(msg string, args ...any) {
-	highlightedMsg := highlightArgs(msg, args...)
+	format, styledArgs := highlightArgs(msg, args...)
 
 	// Print the final formatted message with styled args
-	pterm.Info.Println(highlightedMsg)
+	pterm.Info.Println(fmt.Sprintf(format, styledArgs...))
 }
 
 func Success(msg string, args ...any) {
-	highlightedMsg := highlightArgs(msg, args...)
+	format, styledArgs := highlightArgs(msg, args...)
 
 	// Print the final formatted message with styled args
-	pterm.Info.Println(highlightedMsg)
+	pterm.Info.Println(fmt.Sprintf(format, styledArgs...))
 }
 
 func Warn(msg string, args ...any) {
-	highlightedMsg := highlightArgs(msg, args...)
+	format, styledArgs := highlightArgs(msg, args...)
 
 	// Print the final formatted message with styled args
-	pterm.Warning.Println(highlightedMsg)
+	pterm.Warning.Println(fmt.Sprintf(format, styledArgs...))
 }
 
 func Error(msg string) {
@@ -39,20 +45,16 @@ func Error(msg string) {
 	os.Exit(1)
 }
 
-func highlightArgs(msg string, args ...any) (highlightedMsg string) {
-	style := pterm.NewStyle(pterm.BgYellow, pterm.FgBlack, pterm.Bold)
-
+func highlightArgs(msg string, args ...any) (string, []any) {
 	// Apply the style to each argument
 	styledArgs := make([]any, len(args))
 	for i, arg := range args {
-		styledArgs[i] = style.Sprint(arg)
+		styledArgs[i] = highlightedStyle.Sprint(arg)
 	}
 
 	// As all args are now strings replace format to match
 	regEx := regexp.MustCompile(`%[a-zA-Z]`)
 	format := regEx.ReplaceAllString(msg, "%s")
 
-	highlightedMsg = fmt.Sprintf(format, args...)
-
-	return
+	return format, styledArgs
 }
