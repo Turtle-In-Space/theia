861f32411c62acffaf394a43dd2d07d886f2a9ae
Turtle-In-Space <elias.svensson63@gmail.com>
2025-07-16 00:53:44 +0200

    add smb enumeration command

--- internal/scanners/smbScanner.go
+++ internal/scanners/smbScanner.go
@@ -4,34 +4,55 @@ Copyright © 2025 Elias Svensson <elias.svensson63@gmail.com>
 package scanners
 
 import (
+	"fmt"
+	"os/exec"
+	"path/filepath"
+
 	out "github.com/Turtle-In-Space/theia/pkg/output"
 )
 
-type SMBScanner struct {
+type smbScanner struct {
 	aliases []string
 	name    string
 }
 
 // run the scan on a ipAddr for a port
-func (s SMBScanner) Run(ipAddr string, port int) {
-	out.Info("SMBScanner - %s:%d", ipAddr, port)
+func (s smbScanner) Run(ipAddr string, port int) {
+	txtFile, outFile := s.fileNames(ipAddr, port)
+
+	cmd := exec.Command("enum4linux-ng", "-A", ipAddr, "-oJ", outFile, ">", txtFile)
+	err := cmd.Run()
+
+	if err != nil {
+		out.Error(err.Error())
+	}
 }
 
 // get all aliases for service names
-func (s SMBScanner) Aliases() (aliases []string) {
+func (s smbScanner) Aliases() (aliases []string) {
 	return s.aliases
 }
 
 // get the name of this scanner
-func (s SMBScanner) Name() (name string) {
+func (s smbScanner) Name() (name string) {
 	return s.name
 }
 
 func init() {
-	smbScanner := SMBScanner{
+	smbScanner := smbScanner{
 		aliases: []string{"microsoft-ds", "netbios-ssn"},
 		name:    "SMBScanner",
 	}
 
 	Register("smb", smbScanner)
+}
+
+func (s smbScanner) fileNames(ipAddr string, port int) (txtFile string, outFile string) {
+	txt := fmt.Sprintf("%d_%s.txt", port, s.name)
+	out := fmt.Sprintf("%d_%s.json", port, s.name)
+
+	txtFile = filepath.Join("results", ipAddr, txt)
+	outFile = filepath.Join("xml", ipAddr, out)
+
+	return
 }
