51089e5dc4d4e5192375123d0edcadc094b53634
Turtle-In-Space <elias.svensson63@gmail.com>
2026-05-05 23:01:30 +0200

    add sub dir scan

---
+++ scanners/subdir.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2025 Elias Svensson <elias.svensson63@gmail.com>
+*/
+package scanners
+
+import (
+	"fmt"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/Turtle-In-Space/theia/models"
+)
+
+// ----- Structs ----- //
+
+type subDirScanner struct {
+	serviceNames []string
+	name         string
+}
+
+// ----- Public Functions ----- //
+
+func (s subDirScanner) Run(port models.Port, host models.Host) (err error) {
+	if host.Hostname == "" {
+		return fmt.Errorf("%s: hostname not set, wont run sub dir scan", s.Name())
+	}
+
+	_, dataFileName := fileNames(s.name, ".html", port)
+
+	url := fmt.Sprintf("http://%s:%s", host.Address(), port.ID)
+	wordlist := filepath.Join(seclistPath, "Discovery", "DNS", "subdomains-top1million-110000.txt")
+	hostFuzz := fmt.Sprintf("\"HOST: FUZZ.%s\"", host.Hostname)
+
+	cmd := exec.Command("ffuf", "-u", url, "-w", wordlist, "-ic", "-noninteractive",
+		"-H", hostFuzz, "-ac", "-v", "-of", "html", "-o", dataFileName)
+
+	_, err = execute(s, cmd, "")
+	if err != nil {
+		return fmt.Errorf("%s: %w", s.Name(), err)
+	}
+
+	return nil
+}
+
+func (s subDirScanner) ServiceNames() []string {
+	return s.serviceNames
+}
+
+func (s subDirScanner) Name() (name string) {
+	return s.name
+}
+
+// ----- Private Functions ----- //
+
+func init() {
+	scanner := subDirScanner{
+		serviceNames: []string{"http"},
+		name:         "SubDirScanner",
+	}
+
+	register("subDir", scanner)
+}
